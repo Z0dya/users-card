e449992a01ad8966e3ce954f81a6695f020a08c1
Лазуткин Владислав <vladislavlazutkin64@gmail.com>
2022-11-21 16:29:37 +0300

    fix call api on every page

--- src/store/vuex.js
+++ src/store/vuex.js
@@ -9,15 +9,22 @@ export default new Vuex.Store({
 	state: {
 		// данные из API
 		users: [],
+		fetchUserStatus: 'pending',
 	},
 	// получать состояния из state ()ё
 	getters: {
+		userStatus(state) {
+			return state.fetchUserStatus;
+		},
 		allData(state) {
 			return state.users;
 		},
 	},
 	// функции которые изменяют сосотояние (изменяем\ присваиваем)
 	mutations: {
+		changeStatus(state, newStatus) {
+			state.fetchUserStatus = newStatus;
+		},
 		updateUser(state, updatedUser) {
 			state.users = updatedUser;
 		},
@@ -25,15 +32,20 @@ export default new Vuex.Store({
 	// асинхронные действия и побочки
 	actions: {
 		async fetchUsers(context) {
-			try {
-				const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-				const data = response.data;
-				// отправили полученные данные в mutations
-				context.commit('updateUser', data);
-			} catch (error) {
-				alert(error);
-				console.log(error);
+			if (context.getters.userStatus === 'pending') {
+				try {
+					const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+					const data = response.data;
+					// отправили полученные данные в mutations
+					context.commit('updateUser', data);
+					context.commit('changeStatus', 'done');
+				} catch (error) {
+					alert(error);
+					console.log(error);
+					context.commit('changeStatus', 'error');
+				}
 			}
+			console.log(context.state.fetchUserStatus);
 		},
 	},
 	// разделение и подключение к 1 дереву
